594274fce57860491d558a302e45bec70d446b59
Ronny Perez <ronnypdev@gmail.com>
2020-01-10 08:14:44 -0500

    Implemented All features

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,13 @@
 // Put your applicaiton javascript here
+$(document).ready(function() {
+  $(document).on('change', '#variant-id', function() {
+    let variantId = this.value;
+    let variantImage = $(
+      '.single-product-image[data-variant="' + variantId + '"]'
+    );
+    variantImage
+      .show()
+      .siblings(':visible')
+      .hide();
+  });
+});
